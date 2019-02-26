5e2954f8529d85269ff2657b1f94e52c0966c407
Rogerio Black <rogeriodevelop@gmail.com>
2019-02-25 21:05:27 -0300

    fim do if

--- exemplo-01.php
+++ exemplo-01.php
@@ -1,6 +1,6 @@
 <?php
 
-	$qualSuaIdade = 30;
+	$qualSuaIdade = 125;
 	$idadeCrianca = 12;
 	$idadeMaior = 18;
 	$idadeMelhor = 65;
@@ -10,4 +10,20 @@
 		
 		echo "Criança";
 
+	}else if($qualSuaIdade < $idadeMaior){
+
+		echo "Adolescente";
+
+	}else if ($qualSuaIdade < $idadeMelhor) {
+		echo "Adulto";
+	}else{
+
+		echo "Idoso";
+
+
 	}
+
+
+	echo "<br/>";
+
+	echo ($qualSuaIdade < $idadeMaior)?"Menor de idade":"Maior de idade";
